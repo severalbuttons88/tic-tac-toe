19d26150db3b4ab95139530519ab6a55581c9d9c
Severalbuttons88 <44512019+severalbuttons88@users.noreply.github.com>
2023-02-05 00:00:26 -0600

    Remove unused functions

--- script.js
+++ script.js
@@ -18,39 +18,33 @@ const player = () => {
   ];
   const setName = (nameValue) => {
     name = nameValue;
-
-  }
+  };
   const wonRound = () => {
     totalWon += 1;
   };
+
   const getTotalWins = () => totalWon;
+
   const resetWins = () => {
     totalWon = 0;
   };
-  const getRounds = () => totalWon;
 
   const getName = () => name;
+
   const setSymbol = (appliedSymbol) => {
     symbol = appliedSymbol;
   };
   const getSymbol = () => symbol;
 
   const getSelections = () => playerSelections;
+
   const addSelectedId = (id) => {
     selectedId = id;
   };
   const getSelectedId = () => selectedId;
-  const addSelection = (value, index) => {
-    let combinedArray = playerSelections.flat();
-    combinedArray.splice(index, 1, value);
-    separatedArray = [];
-    while (combinedArray.length)
-      separatedArray.push(combinedArray.splice(0, 3));
-    playerSelections = separatedArray;
-    return separatedArray;
-  };
 
   const getTurn = () => myTurn;
+
   const setTurn = (turn) => {
     if (turn === false) {
       myTurn = false;
@@ -65,40 +59,28 @@ const player = () => {
     setName,
     getSelections,
     setTurn,
-    addSelection,
     getTurn,
     getName,
     setSymbol,
     getSymbol,
     wonRound,
-    getRounds,
     resetWins,
     addSelectedId,
     getSelectedId,
     getTotalWins,
-
   };
 };
 const gameBoard = (() => {
   const selectionZones = document.querySelectorAll(".selection");
   const playerOneDiv = document.querySelector(".player1");
   const playerTwoDiv = document.querySelector(".player2");
-
   let lastUsedID = "";
-  let currentRound = 0;
   let gameState = [
     ["", "", ""],
     ["", "", ""],
     ["", "", ""],
   ];
   let hasWon = false;
-  const changeDiv = (player, value) => {
-    if ((player = 1)) {
-      playerOneDiv.setAttribute("id", `${value}`);
-    } else if ((player = 2)) {
-      playerTwoDiv.setAttribute("id", `${value}`);
-    }
-  };
 
   function renderBoard() {
     let combinedState = [].concat(...gameState);
@@ -107,7 +89,7 @@ const gameBoard = (() => {
       selectedCell.textContent = `${cell}`;
     });
   }
-  let getWinState = () => hasWon;
+
   let resetWinState = () => {
     hasWon = false;
   };
@@ -134,8 +116,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
-
+        gameFlow.updateScore();
       } else if (
         (player[1][0] === `${symbol}`) &
         (player[1][1] === `${symbol}`) &
@@ -144,7 +125,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[2][0] === `${symbol}`) &
         (player[2][1] === `${symbol}`) &
@@ -153,7 +134,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[0][0] === `${symbol}`) &
         (player[1][0] === `${symbol}`) &
@@ -162,7 +143,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[0][1] === `${symbol}`) &
         (player[1][1] === `${symbol}`) &
@@ -171,7 +152,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[0][2] === `${symbol}`) &
         (player[1][2] === `${symbol}`) &
@@ -180,7 +161,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[0][0] === `${symbol}`) &
         (player[1][1] === `${symbol}`) &
@@ -189,7 +170,7 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       } else if (
         (player[2][0] === `${symbol}`) &
         (player[1][1] === `${symbol}`) &
@@ -198,15 +179,10 @@ const gameBoard = (() => {
         playerObj.wonRound();
         hasWon = true;
         console.log("win");
-        gameFlow.updateScore()
+        gameFlow.updateScore();
       }
     }
   }
-
-  const exportWin = (playerObj, symbol) => {
-    checkForWin(playerObj, symbol);
-  };
-  const exportState = () => gameState;
 
   function changeGameState(playerInput, symbol) {
     let combinedArray = [].concat(...gameState);
@@ -223,29 +199,24 @@ const gameBoard = (() => {
     gameState = separatedArray;
     renderBoard();
   }
-  const getRound = () => currentRound;
-  const setRound = (round) => {
-    currentRound = round;
-  };
   const getClickedZone = (player1, player2) => {
     for (let i = 0; i < selectionZones.length; i++) {
       selectionZones[i].addEventListener("mousedown", (e) => {
         playerOneWinCount.textContent = `${player1.getTotalWins()}`;
         playerTwoWinCount.textContent = `${player2.getTotalWins()}`;
         if (e.target.textContent === "") {
-
           let id = e.target.id;
           if (hasWon === true) {
             gameFlow.runRound(player1, player2);
-           
+
             hasWon = false;
-        }
+          }
           if (player1.getTurn() === true) {
             player1.addSelectedId(id);
             changeGameState(player1.getSelectedId(), player1.getSymbol());
             renderBoard();
             checkForWin(player1, player1.getSymbol(), gameState);
-            
+
             player2.setTurn(true);
             player1.setTurn(false);
           } else if (player2.getTurn() === true) {
@@ -260,60 +231,23 @@ const gameBoard = (() => {
       });
     }
   };
-  const runState = (playerInput, symbol) => {
-    changeGameState(playerInput, symbol);
-  };
-  const getLastId = () => lastUsedID;
-
-  const removeClickedZone = (indexToRemove) => {
-    selectionZones[indexToRemove].removeEventListener("mousedown", (e) => {
-      if (e.target.textContent === "") {
-        if (e.target.textContent === "") {
-          let id = e.target.id;
-          if (player1.getTurn() === true) {
-            player1.addSelectedId(id);
-            changeGameState(player1.getSelectedId(), player1.getSymbol());
-            renderBoard();
-            checkForWin(player1, player1.getSymbol());
-            player2.setTurn(true);
-            player1.setTurn(false);
-          } else if (player2.getTurn() === true) {
-            player2.addSelectedId(id);
-            changeGameState(player2.getSelectedId(), player2.getSymbol());
-            renderBoard();
-            checkForWin(player2, player2.getSymbol());
-            player1.setTurn(true);
-            player2.setTurn(false);
-          }
-        }
-      }
-    });
-  };
 
   return {
     changeGameState,
     getClickedZone,
-    removeClickedZone,
-    getRound,
-    setRound,
-    changeDiv,
     defaultBoard,
-    getLastId,
-    runState,
-    exportWin,
-    exportState,
-    getWinState,
     resetWinState,
   };
 })();
+
 const gameFlow = (() => {
-    let playerOneName = playerOneNameDiv.value;
-    let playerTwoName = playerTwoNameDiv.value;
-    let gameStarted = false;
-    const playerOne = player();
-    const playerTwo = player();
+  let playerOneName = playerOneNameDiv.value;
+  let playerTwoName = playerTwoNameDiv.value;
+  let gameStarted = false;
+  const playerOne = player();
+  const playerTwo = player();
   const startButton = document.querySelector("#start");
-  const resetButton = document.querySelector("#reset")
+  const resetButton = document.querySelector("#reset");
   const defaultState = [
     ["", "", ""],
     ["", "", ""],
@@ -322,8 +256,7 @@ const gameFlow = (() => {
   const updateScore = () => {
     playerOneWinCount.textContent = `${playerOne.getTotalWins()}`;
     playerTwoWinCount.textContent = `${playerTwo.getTotalWins()}`;
-    
-  }
+  };
   function startGameButton() {
     startButton.addEventListener("click", () => {
       setDefault(playerOne, playerTwo);
@@ -334,15 +267,13 @@ const gameFlow = (() => {
       gameStarted = false;
       createRound(playerOne, playerTwo);
       updateScore();
-
     });
   }
   function resetGameButton() {
     resetButton.addEventListener("click", () => {
-        setDefault(playerOne, playerTwo);
-        updateScore();
-    })
-
+      setDefault(playerOne, playerTwo);
+      updateScore();
+    });
   }
   function setDefault(player1, player2) {
     playerOneName = playerOneNameDiv.value;
@@ -356,14 +287,14 @@ const gameFlow = (() => {
     player1.setSymbol("x");
     player2.setSymbol("o");
     if (gameStarted === false) {
-    player1.resetWins();
-    player2.resetWins();
-    gameStarted = true;
+      player1.resetWins();
+      player2.resetWins();
+      gameStarted = true;
     }
     gameBoard.defaultBoard();
   }
   function createRound(player1, player2) {
-    runRound(player1, player2)
+    runRound(player1, player2);
   }
   const runRound = (player1, player2) => {
     setDefault(player1, player2);
@@ -371,5 +302,5 @@ const gameFlow = (() => {
   };
   startGameButton();
   resetGameButton();
-  return {runRound, updateScore};
+  return { runRound, updateScore };
 })();
